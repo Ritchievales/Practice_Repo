d711314ffdc65ed8bbd3f2d6175f77dd6ad8ddb4
ririos <ricardo.rios@oracle.com>
2022-06-13 14:06:49 -0500

    first commit

---
+++ Leetcode Practices/src/removeElement/RemoveElement.java
@@ -0,0 +1,20 @@
+package removeElement;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+        int a;
+        int last = nums.length-1;
+        int k=0;
+        for(int i=nums.length-1;i>=0;i--){
+           if(nums[i]==val){
+               a = nums[i]; 
+               nums[i] = nums[last]; 
+               nums[last] = a; 
+               
+               last--; 
+               k++; 
+           }
+        }
+        return nums.length-k;
+    }
+}
